f2f7e1778284894f4f4f57b9b4685d5abb7759c0
qasim-gondal <qgondal48@gmail.com>
2024-08-05 22:57:29 +0500

    Initial Commit

---
+++ wallatshareios/wallatshareiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallatshareiosApp.swift
+//  wallatshareios
+//
+//  Created by Muhammad Qasim on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct wallatshareiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
